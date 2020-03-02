8acb5b63adcf2e8cc43cc8a22379d5dcb5eadee5
JonatanGonzalez09 <jonatan0924@gmail.com>
2020-03-02 13:08:33 -0500

    getBluePrintsByAuthor terminado

--- BLUEPRINTS_PART1/src/main/resources/static/js/apimock.js
+++ BLUEPRINTS_PART1/src/main/resources/static/js/apimock.js
@@ -1,5 +1,77 @@
 /* En este agregue más planos (con más puntos) a los autores 'quemados' en el código. */
 
-var apimocks = (function () {
-    // code
-  })();
+var apimock = (function () {
+
+  var mockdata = [];
+
+  mockdata["JhonConnor"] = [
+      {
+          author: "JhonConnor",
+          name: "house",
+          points: [
+              {
+                  x: 100,
+                  y: 150
+              },
+              {
+                  x: 220,
+                  y: 145
+              },
+              {
+                  x: 245,
+                  y: 200
+              },
+              {
+                  x: 50,
+                  y: 50
+              },
+              {
+                  x: 90,
+                  y: 25
+              },
+              {
+                  x: 10,
+                  y: 70
+              },
+              {
+                x: 20,
+                y: 30
+              },
+              {
+                x: 35,
+                y: 75
+              },
+              {
+                x: 250,
+                y: 70
+              },
+              {
+                x: 110,
+                y: 185
+              }
+          ]
+      },
+      {
+          author: "JhonConnor",
+          name: "gear",
+          points: [
+              {
+                  x: 100,
+                  y: 150
+              },
+              {
+                  x: 200,
+                  y: 250
+              }
+          ]
+      }
+  ]
+
+
+  return {
+      getBlueprintsByAuthor: function(author, callback) {
+          callback(null, mockdata[author]);
+      },
+  }
+
+})();
--- BLUEPRINTS_PART1/src/main/resources/static/js/app.js
+++ BLUEPRINTS_PART1/src/main/resources/static/js/app.js
@@ -2,4 +2,51 @@
 
 var app = (function () {
 
-  })();
+  var getBlueprintsByAuthor = function (author) {
+      return apimock.getBlueprintsByAuthor(author, function (err, data) {
+          if (err) {
+              return new Error("Error obteniendo los blueprints:" + err)
+          }
+          console.log("lista: "+apimock.getBlueprintsByAuthor);
+          console.log("data: "+data.length);
+          console.log("data: "+data[0].name);
+          console.log("data: "+data[1].name);
+          console.log("author: " +author);
+
+          $('#tablaAuthor').html(author + "'s blueprints");
+          var table = $('#bluePrints')
+          table.empty();
+          table.append(`
+          <thead>
+              <tr>
+                  <th>Blueprint name</th>
+                  <th>Number of points</th>
+                  <th>Open</th>
+              </tr>
+          </thead>
+          `)
+          var totalPoints = 0;
+          for(var i = 0; i < data.length; i++){
+              table.append(`
+              <tr>
+                  <td>` + data[i].name + `</td>
+                  <td>` + data[i].points.length + `</td>
+                  <td>
+                      <button type="button" class="btn btn-info">Open</button>
+                      </button>
+                  </td>
+              </tr>
+              `)
+              totalPoints = totalPoints + data[i].points.length;
+          }
+          table.append('</tbody>');
+          $('#puntosTotales').html('Total user points: ' + totalPoints);
+          
+      });
+  }
+
+  return {
+    getBlueprintsByAuthor: getBlueprintsByAuthor,
+  }
+
+})();
